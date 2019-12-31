1daa8d84d5c9751e3957319702c1f958158b2ec8
thiagooliveira <thiagooliveira@digix.com.br>
2019-12-31 17:01:48 -0400

    Comunicando com o banco do firebase

---
+++ configuracao-firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCV_Utr9QyVsGG63mD8rZdYXy0xZdxVu4Q",
+  authDomain: "thiago-dsign.firebaseapp.com",
+  databaseURL: "https://thiago-dsign.firebaseio.com",
+  projectId: "thiago-dsign",
+  storageBucket: "thiago-dsign.appspot.com",
+  messagingSenderId: "17855283546",
+  appId: "1:17855283546:web:55607829a31d26ced33363"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
