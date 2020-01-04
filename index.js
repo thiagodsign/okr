311c1f5c8fdcc81727eb53a783c60d80316e764c
thiagooliveira <thiagooliveira@digix.com.br>
2020-01-04 11:06:06 -0400

    Removendo o firebase na bagaça e iniciando a utilização de modulos exports no js

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
-let okr = [{
-  quarter: [{
-    id: 1,
-    nome: 'Quarter 01',
-    objetivos: [{
-      nome: 'Objetivo Nome',
-      descricao: 'Objetivo Descrição',
-      krs: [{
-        nome: 'Tornar 4x',
-        valorInicial: 40,
-        valorFinal: 60,
-        valorAtual: 55
-      },
-      {
-        nome: 'Tornar 4x',
-        valorInicial: 40,
-        valorFinal: 60,
-        valorAtual: 55
-      }]
-    }]
-  },
-
-  {
-    id: 2,
-    nome: 'Quarter 02',
-    objetivos: [{
-      nome: 'Objetivo Nome',
-      descricao: 'Objetivo Descrição',
-      krs: [{
-        nome: 'Tornar 4x',
-        valorInicial: 40,
-        valorFinal: 60,
-        valorAtual: 55
-      }]
-    }]
-  }]
-}]
-
+import { okr } from "./okr.js"
 
 new Vue({
   el: '#itemObjetivo',
